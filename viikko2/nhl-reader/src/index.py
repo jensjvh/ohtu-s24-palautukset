9a089cc5219d47230e75cc0c1fdc302749b4ba1f
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-11-11 22:38:46 +0200

    Add GUI for player list

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,5 +1,9 @@
 import requests
 from player import Player
+from rich import print as rich_print
+from rich.prompt import Prompt
+from rich.table import Table
+from rich.console import Console
 
 class PlayerReader:
     def __init__(self, url):
@@ -23,13 +27,33 @@ class PlayerStats:
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
     reader = PlayerReader(url)
-    nationality = "FIN"
-    players = PlayerStats(reader).top_scorers_by_nation(nationality)
 
-    print("Players from FIN:")
+    rich_print("NHL statistics by nationality")
 
-    for player in players:
-        print(player)
+    while True:
+        season = Prompt.ask("Select season [2018-19/2019-20/0220-21/2021-22/2022-23/2023-24/2024-25/]")
+        if season == "":
+            break
+
+        while True:
+            nationality = Prompt.ask("Select nationality [AUT/CZE/AUS/SWE/GER/DEN/SUI/SVK/NOR/RUS/CAN/LAT/BLR/SLO/USA/FIN/GBR/]")
+            if nationality == "":
+                break
+
+            table = Table(title=f'Top scorers of {nationality} season {season}')
+            table.add_column('name')
+            table.add_column('team')
+            table.add_column('goals')
+            table.add_column('assists')
+            table.add_column('points')
+
+            players = PlayerStats(reader).top_scorers_by_nation(nationality)
+
+            for player in players:
+                table.add_row(player.name, player.team, str(player.goals), str(player.assists),
+                              str(player.goals + player.assists))
+                
+            rich_print(table)
 
 if __name__ == "__main__":
     main()
