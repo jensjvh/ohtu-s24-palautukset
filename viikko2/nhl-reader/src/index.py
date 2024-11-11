dd64cbce10fe15bc835e7f953743f3ba3c92bd3a
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-11-11 22:15:04 +0200

    Refactor player list

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,13 +1,30 @@
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_players(self):
+        response = requests.get(self._url).json()
+        return [Player(player_dict) for player_dict in response]
+    
+class PlayerStats:
+    def __init__(self, reader):
+        self._reader = reader
+    
+    def top_scorers_by_nation(self, nationality):
+        players = self._reader.get_players()
+        filtered_players = [player for player in players
+                            if player.nationality == nationality]
+        filtered_players.sort(reverse=True, key=lambda player: player.points)
+        return filtered_players
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
-
-    players = [Player(player_dict) for player_dict in response if player_dict['nationality'] == 'FIN']
-
-    players.sort(reverse=True,key=lambda player: player.points)
+    reader = PlayerReader(url)
+    nationality = "FIN"
+    players = PlayerStats(reader).top_scorers_by_nation(nationality)
 
     print("Players from FIN:")
 
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -2,6 +2,7 @@ class Player:
     def __init__(self, dict):
         self.name = dict['name']
         self.team = dict['team']
+        self.nationality = dict['nationality']
         self.goals = dict['goals']
         self.assists = dict['assists']
         self.points = self.goals + self.assists
