2d86ffa1996c8cd0c4337fec34d92f41eaec4016
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-11-11 21:21:53 +0200

    Change in clone 2

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -7,6 +7,9 @@ from erotus import erotus
 logger("aloitetaan ohjelma") # muutos mainissa
 
 x = int(input("luku 1: ")) # muutos ensimmäisessä kloonissa
+
+# muutos toisessa kloonissa
+x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 print(f"{summa(x, y)}")
 print(f"{erotus(x, y)}")
