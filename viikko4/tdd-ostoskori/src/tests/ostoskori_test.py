ab9aa2969c0a9f8c2b3eaa60cc94fc2c426e4da1
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-11-20 23:30:56 +0200

    Add test for price of two of the same product added to cart

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -20,7 +20,7 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
 
-        self.assertEqual(self.kori.hinta(), 3)
+        self.assertEqual(self.kori.hinta(), maito.hinta())
 
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_2_tavaraa(self):
         maito = Tuote("Maito", 3)
@@ -36,7 +36,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(porkkana)
 
-        self.assertEqual(self.kori.hinta(), 7)
+        hintojen_summa = maito.hinta() + porkkana.hinta()
+
+        self.assertEqual(self.kori.hinta(), hintojen_summa)
     
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_2_tavaraa(self):
         maito = Tuote("Maito", 3)
@@ -44,3 +46,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korin_hinta_on_2_kertaa_tuotteen_hinta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), maito.hinta() * 2)
