0d6b83aade2b788dde5508fc0bd6484b54727078
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-11-20 23:26:42 +0200

    Add test for price of two different products added to cart

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -29,3 +29,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(porkkana)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_sama_kuin_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        porkkana = Tuote("Porkkana", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(porkkana)
+
+        self.assertEqual(self.kori.hinta(), 7)
