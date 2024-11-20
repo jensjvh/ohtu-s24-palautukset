f35eed116047f20d491f54aab7a9d36ed916a2a8
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-11-20 23:58:23 +0200

    Change method `poista_tuote()` to decrement product amount in cart

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -101,3 +101,14 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(ostos.tuotteen_nimi(), maito.nimi())
         self.assertEqual(ostos.lukumaara(), 2)
+    
+    def test_jos_korissa_2_samaa_tuotetta_toinen_poistetaan_koriin_jää_ostos_jossa_tuotetta_1(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        
+        self.kori.poista_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+
+        self.assertEqual(ostos.lukumaara(), 1)
