ebba74a1c872d74de739f2da4e1962487c1e7f88
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-12-09 19:25:28 +0200

    Add exercise 5 part 2

--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -11,6 +11,9 @@ from matchers import (
 
 class QueryBuilder:
     def __init__(self):
+        self._reset()
+
+    def _reset(self):
         self._matcher = All()
 
     def plays_in(self, team):
@@ -22,9 +25,18 @@ class QueryBuilder:
     def has_fewer_than(self, value, attr):
         return self._add_matcher(HasFewerThan(value, attr))
 
+    def has_not(self, matcher):
+        return self._add_matcher(Not(matcher))
+
+    def one_of(self, *matchers):
+        self._matcher = (Or(*matchers))
+        return self
+
     def _add_matcher(self, matcher):
         self._matcher = And(self._matcher, matcher)
         return self
 
     def build(self):
-        return self._matcher
+        result = self._matcher
+        self._reset()
+        return result
